3a2f562d2c0f1e70cb84261808d22560c08f4970
Chandru <65664480+B-chandru@users.noreply.github.com>
2020-12-26 14:01:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function send(){
-         navigator.vibrate([500, 250, 500, 250, 500, 250, 500, 250, 500, 250, 500]);
+        var input =document.getElementById("input").value;
+       if(input==="" || input=== NaN){
+          alert("Enter any number")
+}else{
+         navigator.vibrate([500, input, 500, 250, 300, 250, 400, 250, input, 250, 500]);
     }
-
+}
 function stop() {
     navigator.vibrate(0);
 }
