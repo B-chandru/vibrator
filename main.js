4bf02af80678aeb77f64ffe3daee62555a930788
Chandru <65664480+B-chandru@users.noreply.github.com>
2021-02-28 19:46:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function send(){
         var input =document.getElementById("input").value;
-       if(input==="" || input === NaN){
+       if(input==="" && input === NaN){
           alert("Enter any number")
 }else{
          navigator.vibrate([500, input, 500, 250, 300, 250, 400, 250, input, 250, 500])
